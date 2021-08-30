ac57d97b9e0c71964129ae841fc6bdfe4485db87
Mike Aparicio <peruvianidol@gmail.com>
2021-08-30 15:51:46 -0500

    Log subscriber

--- netlify/functions/twitch-eventsub.js
+++ netlify/functions/twitch-eventsub.js
@@ -1,12 +1,22 @@
 exports.handler = async (event) => {
+
   const { headers = [] } = event;
   const type = headers['twitch-eventsub-message-type'] || 'no type';
+  const eventType = headers['twitch-eventsub-subscription-type'];
+  if (type !== 'notification' || eventType !== 'channel.subscribe') {
+    return { statusCode: 200, body: 'ok'};
+  }
+  console.log({ headers: event.headers, body: event.body });
+  const { event: twitchEvent } = JSON.parse(event.body);
+  const user = twitchEvent.user_name;
+
+  console.log(`${user} just subscribed!`);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       type,
-      message: 'ok',
+      message: `${user} just subscribed!`,
     }),
   };
 };

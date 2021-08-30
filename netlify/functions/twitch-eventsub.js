02f217e78d93069e1e7590e6b5410d4d2d35c833
Mike Aparicio <peruvianidol@gmail.com>
2021-08-30 14:24:58 -0500

    remove ascii boi

--- netlify/functions/twitch-eventsub.js
+++ netlify/functions/twitch-eventsub.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   const { headers = [] } = event;
-  const type = headers['twitch-eventsub-message-type'] ?? 'no type';
+  const type = headers['twitch-eventsub-message-type'] || 'no type';
 
   return {
     statusCode: 200,

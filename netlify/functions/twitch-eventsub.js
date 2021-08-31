a5b1bfb958127943a7d6307a91f6a97804499621
Mike Aparicio <peruvianidol@gmail.com>
2021-08-31 09:06:07 -0500

    Add channel online and follow subscriptions

--- netlify/functions/twitch-eventsub.js
+++ netlify/functions/twitch-eventsub.js
@@ -1,12 +1,15 @@
 exports.handler = async (event) => {
-
   const { headers = [] } = event;
+
   const type = headers['twitch-eventsub-message-type'] || 'no type';
   const eventType = headers['twitch-eventsub-subscription-type'];
+
   if (type !== 'notification' || eventType !== 'channel.subscribe') {
-    return { statusCode: 200, body: 'ok'};
+    return { statusCode: 200, body: ''};
   }
+
   console.log({ headers: event.headers, body: event.body });
+
   const { event: twitchEvent } = JSON.parse(event.body);
   const user = twitchEvent.user_name;
 
